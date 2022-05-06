12e47094914d03af687c6880e956977bb5948722
LuaanBarbosa <luancesar@outlook.com>
2022-05-06 08:34:33 -0300

    Ajustes no layout

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,19 +1,12 @@
-
 let v = document.getElementById("bg_video");
 
-window.onload = Set_VideoRate ();
+window.onload = Set_VideoRate();
 
-function Set_VideoRate() { 
+function Set_VideoRate() {
     console.log(v);
     v.playbackRate = 0.5;
-} 
+}
 
 // const ano = document.getElementById("ano");
 // const anoAtual = new Date();
 // ano.innerHTML = anoAtual.getFullYear();
-
-
-
-
-
-
